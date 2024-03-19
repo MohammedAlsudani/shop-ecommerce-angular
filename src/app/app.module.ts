b2b59e294573956a4309480a125f6f4cfba303c4
mohammed <alsudanimohammed1@gmail.com>
2024-03-19 15:21:23 +0200

    starter app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,23 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+import { ProductService } from './services/product.service';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    ProductService
   ],
   bootstrap: [AppComponent]
 })
